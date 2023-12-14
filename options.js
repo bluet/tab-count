97e6d39da9f7dd25254cd0e90c095b2860fa18dc
BlueT - Matthew Lien - 練喆明 <bluet@bluet.org>
2023-12-14 09:46:50 +0800

    added: find duplicate tabs and bulk close

--- options.js
+++ options.js
@@ -43,16 +43,16 @@ document.addEventListener("DOMContentLoaded", restore_options);
 // Add event listeners to the radio buttons
 const radios = document.getElementById("popupOptionsForm").tabCountRadios;
 for (let i = 0; i < radios.length; i++) {
-	radios[i].addEventListener("click", async () => {return await save_options("badgeDisplayOption", radios[i].value);});
+	radios[i].addEventListener("click", async () => { return await save_options("badgeDisplayOption", radios[i].value); });
 }
 
 // Add event listener for tabDedupe checkbox.
 const checkbox = document.getElementById("tabDedupe");
-checkbox.addEventListener("click", async () => {return await save_options("tabDedupe", checkbox.checked);});
+checkbox.addEventListener("click", async () => { return await save_options("tabDedupe", checkbox.checked); });
 
 // Add event listener for tabJanitor checkbox.
 const janitorCheckbox = document.getElementById("tabJanitor");
-janitorCheckbox.addEventListener("click", async () => {return await save_options("tabJanitor", janitorCheckbox.checked);});
+janitorCheckbox.addEventListener("click", async () => { return await save_options("tabJanitor", janitorCheckbox.checked); });
 
 // Add event listener for tabJanitorDays input.
 document.getElementById("tabJanitorDays").addEventListener("input", () => {
@@ -98,3 +98,138 @@ async function updateBadgeTitle (count) {
 	const iconTitle = `You have ${count} open tab(s)/window(s).`;
 	await chrome.action.setTitle({ "title": iconTitle });
 }
+
+
+// find duplicate tabs
+document.getElementById("scanDuplicateTabsButton").addEventListener("click", async () => {
+	const tabs = await chrome.tabs.query({});
+	const windows = await chrome.windows.getAll({});
+	const urlTabMap = new Map();
+
+	for (let tab of tabs) {
+		if (!urlTabMap.has(tab.url)) {
+			urlTabMap.set(tab.url, []);
+		}
+		urlTabMap.get(tab.url).push(tab);
+	}
+
+	// Remove the table if it already exists
+	const existingTable = document.getElementById("duplicateTabsTable");
+	if (existingTable) {
+		existingTable.remove();
+	}
+
+	// Create a table to display the duplicate tabs
+	const table = document.createElement("table");
+	table.id = "duplicateTabsTable";
+	table.innerHTML = `
+	    <tr>
+		<th></th>
+		<th>Favicon</th>
+		<th>Title</th>
+		<th>Window Index</th>
+		<th>Action</th>
+	    </tr>
+	`;
+
+	for (let [url, tabs] of urlTabMap) {
+		if (tabs.length > 1) {
+			for (let tab of tabs) {
+				const row = document.createElement("tr");
+
+				// Add checkbox
+				const checkboxCell = document.createElement("td");
+				const checkbox = document.createElement("input");
+				checkbox.type = "checkbox";
+				checkbox.value = tab.id;
+				checkboxCell.appendChild(checkbox);
+				row.appendChild(checkboxCell);
+
+				// Add favicon
+				const faviconCell = document.createElement("td");
+				const favicon = document.createElement("img");
+				favicon.src = tab.favIconUrl;
+				faviconCell.appendChild(favicon);
+				row.appendChild(faviconCell);
+
+				// Add title
+				const titleCell = document.createElement("td");
+				titleCell.textContent = tab.title;
+				row.appendChild(titleCell);
+
+				// Add window index
+				const windowIndexCell = document.createElement("td");
+				const windowIndex = windows.findIndex((window) => { return window.id === tab.windowId; });
+				windowIndexCell.textContent = windowIndex;
+				row.appendChild(windowIndexCell);
+
+				// Add close button
+				const actionCell = document.createElement("td");
+				const closeButton = document.createElement("button");
+				closeButton.textContent = "Close Tab";
+				closeButton.addEventListener("click", () => {
+					chrome.tabs.remove(tab.id);
+					row.remove(); // Remove the row from the table
+				});
+				actionCell.appendChild(closeButton);
+				row.appendChild(actionCell);
+
+				// Set the title attribute to the tab's URL
+				row.title = tab.url;
+
+				// Add a click event listener to switch to the tab
+				row.addEventListener("dblclick", () => {
+					chrome.tabs.update(tab.id, { "active": true });
+					chrome.windows.update(tab.windowId, { "focused": true });
+				});
+
+				table.appendChild(row);
+			}
+		}
+	}
+
+	// Append the table to the body of the options page
+	document.body.appendChild(table);
+
+	// "auto-select" button
+	document.getElementById("autoSelectButton").addEventListener("click", () => {
+		const checkboxes = Array.from(table.querySelectorAll("input[type='checkbox']"));
+		const urlCheckboxMap = new Map();
+
+		for (let checkbox of checkboxes) {
+			const url = checkbox.parentElement.parentElement.title;
+			if (!urlCheckboxMap.has(url)) {
+				urlCheckboxMap.set(url, []);
+			}
+			urlCheckboxMap.get(url).push(checkbox);
+		}
+
+		for (let [url, urlCheckboxes] of urlCheckboxMap) {
+			if (urlCheckboxes.length > 1) {
+				for (let i = 1; i < urlCheckboxes.length; i++) {
+					urlCheckboxes[i].checked = true;
+				}
+			}
+		}
+	});
+
+
+	// "bulk close" button
+	document.getElementById("bulkCloseButton").addEventListener("click", () => {
+		const checkboxes = table.querySelectorAll("input[type='checkbox']");
+		for (let checkbox of checkboxes) {
+			if (checkbox.checked) {
+				chrome.tabs.remove(parseInt(checkbox.value));
+				checkbox.parentElement.parentElement.remove(); // Remove the row from the table
+			}
+		}
+
+		const existingTable = document.getElementById("duplicateTabsTable");
+		if (existingTable) {
+			existingTable.remove();
+		}
+	});
+});
+
+
+
